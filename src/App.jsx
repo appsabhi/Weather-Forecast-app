30a61b1fb981b8a20aaabee8ec3d6de38819899b
Abhinand P <abhinandappuabhinand@gmail.com>
2024-11-04 14:14:05 +0530

    updated with adding api fetch functionality

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Weather from "./components/Weather"
+
+const App = () => {
+  return (
+    <>
+    <div className="w-full h-full flex justify-center items-center ">
+    <Weather/>
+    </div>
+    </>
+  )
+}
+
+export default App
